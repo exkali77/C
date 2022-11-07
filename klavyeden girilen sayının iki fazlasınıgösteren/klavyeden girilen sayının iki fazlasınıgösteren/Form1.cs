57a1ff3a6b6ae6aa7d0154ed87c1378640051c27
Kerem Şenses <68199514+exkali77@users.noreply.github.com>
2022-11-07 21:42:24 +0300

    Proje
    Girilen yasının iki fazlasını bulan program

---
+++ klavyeden girilen sayının iki fazlasınıgösteren/klavyeden girilen sayının iki fazlasınıgösteren/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace klavyeden_girilen_sayının_iki_fazlasınıgösteren
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int s1,s3;
+            s1 = Convert.ToInt32(textBox1.Text);
+
+            s3 = s1 + 2;
+            label3.Text = s3.ToString();
+
+            label1.Location.X();
+        }
+    }
+}
